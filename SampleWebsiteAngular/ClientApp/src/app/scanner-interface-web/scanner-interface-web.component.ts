0ff12f219b1cd319f19d51c34ff448b328499fdf
tinhngo-saigontechnology <tinh.ngo@saigontechnology.com>
2024-02-19 10:21:12 +0700

    fix: spelling error

--- SampleWebsiteAngular/ClientApp/src/app/scanner-interface-web/scanner-interface-web.component.ts
+++ SampleWebsiteAngular/ClientApp/src/app/scanner-interface-web/scanner-interface-web.component.ts
@@ -24,7 +24,7 @@ export class ScannerInterfaceWebComponent implements OnInit {
           responseMessage = {
             filename: response.filename,
             fileSize: `${response.fileLength} (${response.sizeDisplay})`,
-            fileExtention: response.extension
+            fileExtension: response.extension
           };
         }
 
